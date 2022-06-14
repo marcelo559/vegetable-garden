654d4debe6591dc1bfc0d7799c0b7d5f5a251f5e
Marcelo Campos <marcelo559@gmail.com>
2022-06-14 11:25:11 -0300

    File responsible to connect MySQL
    I will use this file to make connections to the database, in this way the connection information will be centralized

---
+++ version-02-SensorData/connection.php
@@ -0,0 +1,14 @@
+<?php
+//I will use this file to make connections to the database
+//The connection information will be centralized
+
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = "REPLACE_WITH_YOUR_DATABASE_NAME";
+// REPLACE with Database user
+$username = "REPLACE_WITH_YOUR_USERNAME";
+// REPLACE with Database user password
+$password = "REPLACE_WITH_YOUR_PASSWORD";
+
+?>
